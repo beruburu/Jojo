dc6ff4780346ae537f134c26638857bce078b38a
Meghan Tius <meghantius@gmail.com>
2016-03-23 16:21:24 -0700

    testimonials fix

--- testimonials.php
+++ testimonials.php
@@ -41,23 +41,23 @@
 			?>
         </div> 
         <header>
-		    <a href="index.html"><h1><img src="Images/logo.png"
+		    <a href="index.php"><h1><img src="Images/logo.png"
                  width="200" height="200" alt="Jojo's Dog Grooming-Home"></h1></a>
 		</header>
         <nav>
             <div class="dropdown">
                 <ul id="nav">
-                    <li><a href="aboutUs.html">About Us</a></li>
-                    <li><a href="testimonials.html">Testimonials</a></li>
-                    <li><a href="services.html" id="drop">Services</a>
+                    <li><a href="aboutus.php">About Us</a></li>
+                    <li><a href="testimonials.php">Testimonials</a></li>
+                    <li><a href="services.php" id="drop">Services</a>
 					    <div class="dropdowncontent">
 						    <ul>
-                                <li><a href="Services/pricing.html">Pricing</a></li>
-					            <li><a href="Services/booking.html">Booking</a></li>
-					            <li><a href="Services/faqs.html">FAQs</a></li>
+                                <li><a href="pricing.php">Pricing</a></li>
+					            <li><a href="booking.php">Booking</a></li>
+					            <li><a href="faqs.php">FAQs</a></li>
                             </ul>
 			            </div></li>
-                    <li><a href="contactus.html">Contact Us</a></li>
+                    <li><a href="contactus.php">Contact Us</a></li>
                 </ul>
             </div>
         </nav>
@@ -94,14 +94,14 @@
 		<footer>
             <div id="footernav">
                 <ul>
-                    <li><a href="signIn.html">Sign in / Register</a></li>
-                    <li><a href="aboutus.html">About Us</a></li>
-                    <li><a href="testimonials.html">Testimonials</a></li>
-                    <li><a href="services.html">Services</a>
-                    <li><a href="Services/pricing.html">Pricing</a></li>
-					<li><a href="Services/booking.html">Booking</a></li>
-					<li><a href="Services/faqs.html">FAQs</a></li>
-                    <li><a href="contactus.html">Contact Us</a></li>
+                    <li><a href="signIn.php">Sign in / Register</a></li>
+                    <li><a href="aboutus.php">About Us</a></li>
+                    <li><a href="testimonials.php">Testimonials</a></li>
+                    <li><a href="services.php">Services</a>
+                    <li><a href="pricing.php">Pricing</a></li>
+					<li><a href="booking.php">Booking</a></li>
+					<li><a href="faqs.php">FAQs</a></li>
+                    <li><a href="contactus.php">Contact Us</a></li>
                 </ul>
             </div>
 		    <p>Follow us:</p>
