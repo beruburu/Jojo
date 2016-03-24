7a18772ff6cc09522b08eccb90cd04a4fd56c7c2
Meghan Tius <meghantius@gmail.com>
2016-03-23 17:03:51 -0700

    login page with proper css

--- login_form.php
+++ login_form.php
@@ -21,25 +21,10 @@
     <body>
         
         <div id="signin">
-            <?php
-			$sql="SELECT * FROM $tbl_name ORDER BY id DESC";
-			$result=mysql_query($sql);
-			$count = min(5, mysql_num_rows($result));
-			if($count==0)
-				echo "<li>No topics</li>";
-			else
-				for($i=0; $i < $count; $i++){
-					$row=mysql_fetch_array($result);
-					echo '<li><a href="view_topic.php?id='.$row['id'].'">'.$row['topic'].'</a></li>';
-				}
-		    ?>
-            <br>
 		    <?php
 			if (isLoggedIn()){
                 echo 'Welcome back, ' . strtoupper($_SESSION['SESS_FIRST_NAME']) . '!<br/>';
 				echo '<a href="logout.php">Logout</a><br/>';
-				echo '<a href="forum.php">Go to forum</a><br/>';
-				echo '>><a href="add_topic_form.php">Create new topic</a>';
 			} else {
 				echo '<a href="login_form.php">Login</a><br/>';
 				echo '<a href="register_form.php">New user?</a>';
@@ -47,23 +32,23 @@
 		    ?>
         </div> 
         <header>
-		    <a href="index.html"><h1><img src="Images/logo.png"
+		    <a href="index.php"><h1><img src="Images/logo.png"
                  width="200" height="200" alt="Jojo's Dog Grooming-Home"></h1></a>
 		</header>
         <nav>
             <div class="dropdown">
                 <ul id="nav">
-                    <li><a href="aboutus.html">About Us</a></li>
-                    <li><a href="testimonials.html">Testimonials</a></li>
-                    <li><a href="services.html" id="drop">Services</a>
+                    <li><a href="aboutus.php">About Us</a></li>
+                    <li><a href="testimonials.php">Testimonials</a></li>
+                    <li><a href="services.php" id="drop">Services</a>
 					    <div class="dropdowncontent">
 						    <ul>
-                                <li><a href="Services/pricing.html">Pricing</a></li>
-					            <li><a href="Services/booking.html">Booking</a></li>
-					            <li><a href="Services/faqs.html">FAQs</a></li>
+                                <li><a href="pricing.php">Pricing</a></li>
+					            <li><a href="booking.php">Booking</a></li>
+					            <li><a href="faqs.php">FAQs</a></li>
                             </ul>
 			            </div></li>
-                    <li><a href="contactus.html">Contact Us</a></li>
+                    <li><a href="contactus.php">Contact Us</a></li>
                 </ul>
             </div>
         </nav>
@@ -74,7 +59,6 @@
 			</div>
 			<div id="rightcol">
 				<!--Regstration form begins here-->
-				<h2>Login</h2>
                     <?php
 	                    if( isset($_SESSION['ERRMSG_ARR']) && is_array($_SESSION['ERRMSG_ARR']) && count($_SESSION['ERRMSG_ARR']) >0 ) {
 		                    echo '<ul class="err">';
@@ -85,22 +69,18 @@
 		                    unset($_SESSION['ERRMSG_ARR']);
 	                    }
                     ?>
-                    <p>&nbsp;</p>
                     <form id="loginForm" name="loginForm" method="post" action="login.php">
-                        <table width="300" border="0" align="center" cellpadding="2" cellspacing="0">
-                            <tr>
-                                <td width="112"><b>Login</b></td>
-                                <td width="188"><input name="login" type="text" class="textfield" id="login" /></td>
-                            </tr>
-                            <tr>
-                                <td><b>Password</b></td>
-                                <td><input name="password" type="password" class="textfield" id="password" /></td>
-                            </tr>
-                            <tr>
-                                <td>&nbsp;</td>
-                                <td><input type="submit" name="Submit" value="Login" /></td>
-                            </tr>
-                        </table>
+						<fieldset>
+							<legend>Sign In</legend>
+							<label for="email">Email: </label><br>
+							<input name="email" type="text" id="login" size="60">
+							<br><br>
+							<label for="pass">Password: </label><br>
+							<input name="pass" type="password" id="pass" size="60">
+							<br><br>
+							<input type="submit" name="Submit" value="Sign In">
+						</fieldest>
+					</form>
             </div>
         </div>
 		<footer id="footer">
@@ -108,13 +88,13 @@
                 <ul>
                     <li><a href="login_form.php">Login</a></li>
                     <li><a href="register_form.php">Register</a></li>
-                    <li><a href="aboutUs.html">About Us</a></li>
-                    <li><a href="testimonials.html">Testimonials</a></li>
-                    <li><a href="services.html">Services</a>
-                    <li><a href="Services/pricing.html">Pricing</a></li>
-					<li><a href="Services/booking.html">Booking</a></li>
-					<li><a href="Services/faqs.html">FAQs</a></li>
-                    <li><a href="contactus.html">Contact Us</a></li>
+                    <li><a href="about.php">About Us</a></li>
+                    <li><a href="testimonials.php">Testimonials</a></li>
+                    <li><a href="services.php">Services</a>
+                    <li><a href="pricing.php">Pricing</a></li>
+					<li><a href="booking.php">Booking</a></li>
+					<li><a href="faqs.php">FAQs</a></li>
+                    <li><a href="contactus.php">Contact Us</a></li>
                 </ul>
             </div>
             <p>Follow us:</p>
