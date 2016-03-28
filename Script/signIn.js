9d1b9684e86e2fb68cee5f88f47e715e891832a8
Haley Booker <haley1652@hotmail.com>
2016-03-27 21:48:32 -0700

    removed on focus functions

--- Script/signIn.js
+++ Script/signIn.js
@@ -67,12 +67,6 @@ function validateFirstName() {
         return true;
     }
 }
-function focusFirstName() {
-    var x = document.getElementById("firstname").value;
-    if (x === null || x === "") {
-        document.getElementById("firstmsg").style.display = "none";
-    }
-}
 function validateLastName() {
     var x = document.getElementById("lastname").value;
     if (x === null || x === "") {
@@ -82,12 +76,6 @@ function validateLastName() {
     else {
         document.getElementById("lastmsg").style.display = "none";
         return true;
-    }
-}
-function focusLastName() {
-    var x = document.getElementById("lastname").value;
-    if (x === null || x === "") {
-        document.getElementById("lastmsg").style.display = "none";
     }
 }
 function validatePhone() {
@@ -105,15 +93,6 @@ function validatePhone() {
         return true;
     }
 }
-function focusPhone() {
-    var x = document.getElementById("phone").value;
-    if (/\d{10}/.test(x) === false) {
-        document.getElementById("phonemsg").style.display = "";
-    }
-    else {
-        document.getElementById("phonemsg").style.display = "none";
-    }
-}
 function validateRegEmail() {
     var x = document.getElementById("registeremail").value;
     if (x === null || x === "") {
@@ -129,12 +108,6 @@ function validateRegEmail() {
         return true;
     }
 }
-function focusRegEmail() {
-    var x = document.getElementById("registeremail").value;
-    if (x === null || x === "") {
-        document.getElementById("emailregmsg").style.display = "none";
-    }
-}
 function validateRegPassword() {
     var x = document.getElementById("pass").value;
     if (x === null || x === "") {
@@ -144,12 +117,6 @@ function validateRegPassword() {
     else {
         document.getElementById("passregmsg").style.display = "none";
         return true;
-    }
-}
-function focusRegPassword() {
-    var x = document.getElementById("pass").value;
-    if (x === null || x === "") {
-        document.getElementById("passregmsg").style.display = "none";
     }
 }
 function validateConPassword() {
@@ -168,12 +135,6 @@ function validateConPassword() {
         return true;
     }
 }
-function focusConPassword() {
-    var x = document.getElementById("confirmpass").value;
-    if (x === null || x === "") {
-        document.getElementById("passconmsg").style.display = "none";
-    }
-}
 function validateDog() {
     var x = document.getElementById("dogname").value;
     if (x === null || x === "") {
@@ -185,12 +146,6 @@ function validateDog() {
         return true;
     }
 }
-function focusDog() {
-    var x = document.getElementById("dogname").value;
-    if (x === null || x === "") {
-        document.getElementById("dogmsg").style.display = "none";
-    }
-}
 function validateBreed() {
     var x = document.getElementById("dogbreed").value;
     if (x === null || x === "") {
@@ -200,11 +155,5 @@ function validateBreed() {
     else {
         document.getElementById("breedmsg").style.display = "none";
         return true;
-    }
-}
-function focusBreed() {
-    var x = document.getElementById("dogbreed").value;
-    if (x === null || x === "") {
-        document.getElementById("breedmsg").style.display = "none";
     }
 }
